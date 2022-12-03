d200b6472c3c5a0493aba2f51d20c809928b98c8
eror2008 <d--g@ukr.net>
2022-12-03 12:17:26 +0200

    main.java was fixed. by hodm 3

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Hello world! squash 1");
         System.out.println("Hello world!2; squash 2");
         System.out.println("Hello world!3; squash 3");
-        System.out.println("Hello world!2; 4");
+        System.out.println("Hello world!2; squash 4");
         System.out.println("Hello world!2; 5");
     }
 }
