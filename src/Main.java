d8d031d8feeed429442e8a5d7b1736d6d99a8cb1
eror2008 <d--g@ukr.net>
2022-12-03 12:10:10 +0200

    main.java was fixed 12/03/2022. by hodm

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Hello world!1");
         System.out.println("Hello world!2; 2");
         System.out.println("Hello world!3; 3");
-        System.out.println("Hello world!2; reb_br_01");
-        System.out.println("Hello world!2; reb_br_01");
+        System.out.println("Hello world!2; 4");
+        System.out.println("Hello world!2; 5");
     }
 }
