331b41d66e8a62ed25fcf88b957b185aa2182a5b
eror2008 <d--g@ukr.net>
2022-12-03 12:07:33 +0200

    main.java was fixed 01.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!1");
         System.out.println("Hello world!2; reb_br_02_01");
         System.out.println("Hello world!3; reb_br_02_01");
         System.out.println("Hello world!2; reb_br_01");
