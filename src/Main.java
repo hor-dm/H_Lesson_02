fd9f58a747303d042fcdda4cd04dbf18aacbd37d
eror2008 <d--g@ukr.net>
2022-12-03 12:17:00 +0200

    main.java was fixed. by hodm 4

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!1");
-        System.out.println("Hello world!2; 2");
-        System.out.println("Hello world!3; 3");
-        System.out.println("Hello world!2; 4");
-        System.out.println("Hello world!2; 5");
+        System.out.println("Hello world! squash 1");
+        System.out.println("Hello world!2; squash 2");
+        System.out.println("Hello world!3; squash 3");
+        System.out.println("Hello world!2; squash 4");
+        System.out.println("Hello world!2; squash 5");
     }
 }
